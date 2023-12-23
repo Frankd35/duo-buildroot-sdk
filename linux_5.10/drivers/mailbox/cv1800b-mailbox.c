cad4fe2ec8e8e849357261028c141e2a528ce111
daiyuntao <d1581209858@live.com>
2023-12-23 20:17:12 +0800

    1.mailbox-test work, but test not pass 2. mailbox changes coding style

--- linux_5.10/drivers/mailbox/cv1800b-mailbox.c
+++ linux_5.10/drivers/mailbox/cv1800b-mailbox.c
@@ -8,73 +8,30 @@
 #include <linux/io.h>
 #include <linux/kfifo.h>
 #include <linux/mailbox_controller.h>
+#include <linux/mailbox_client.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
 
-union cvi_mailbox_info_offset{
-	u8 mbox_info;
-	u32 reserved;
-};
 
-union cvi_mailbox_int_clr_offset{
-	u8 mbox_int_clr;
-	u32 reserved;
-};
-union cvi_mailbox_int_mask_offset{
-	u8 mbox_int_mask;
-	u32 reserved;
-};
-union cvi_mailbox_int_offset{
-	u8 mbox_int;
-	u32 reserved;
-};
-union cvi_mailbox_int_raw_offset{
-	u8 mbox_int_raw;
-	u32 reserved;
-};
-
-union mailbox_set{
-	u8 mbox_set;
-	u32 reserved;
-};
-union mailbox_status{
-	u8 mbox_status;
-	u32 reserved;
-};
-
-union cvi_mailbox_status{
-	u8 mbox_status;
-	u32 reserved;
-};
-
-/* register mapping refers to mailbox user guide*/
-struct cpu_mbox_int {
-	union cvi_mailbox_int_clr_offset  cpu_mbox_int_clr;
-	union cvi_mailbox_int_mask_offset cpu_mbox_int_mask;
-	union cvi_mailbox_int_offset      cpu_mbox_int_int;
-	union cvi_mailbox_int_raw_offset  cpu_mbox_int_raw;
-};
-
-struct mailbox_set_register {
-	union  cvi_mailbox_info_offset cpu_mbox_en[4];      //0x00, 0x04, 0x08, 0x0c
-	struct cpu_mbox_int cpu_mbox_set[4];                //0x10~0x1C, 0x20~0x2C, 0x30~0x3C, 0x40~0x4C
-	u32    reserved[4];                                 //0x50~0x5C
-	union  mailbox_set mbox_set;                        //0x60
-	union  mailbox_status mbox_status;                  //0x64
-	u32    reserved2[2];                                //0x68~0x6C
-	union  cvi_mailbox_status cpu_mbox_status[4];       //0x70
-};
-
-struct mailbox_done_register {
-	union  cvi_mailbox_info_offset cpu_mbox_done_en[4];
-	struct cpu_mbox_int cpu_mbox_done[4];
-};
 
 #define MAILBOX_MAX_CHAN		0x0008
 #define MAILBOX_DONE_OFFSET     0x0002
 #define MAILBOX_CONTEXT_SIZE	0x0040
 #define MAILBOX_CONTEXT_OFFSET  0x0400
+
+#define MBOX_EN_REG(cpu)				(cpu<<2)
+#define MBOX_DONE_REG(cpu)			(cpu<<2) + MAILBOX_DONE_OFFSET
+
+#define MBOX_SET_CLR_REG(cpu) 	0x10 + (cpu<<4) 
+#define MBOX_SET_MSK_REG(cpu) 	0x10 + (cpu<<4) + 4
+#define MBOX_SET_INT_REG(cpu) 	0x10 + (cpu<<4) + 8
+
+#define MBOX_DONE_CLR_REG(cpu) 	MAILBOX_DONE_OFFSET + MBOX_SET_CLR_REG(cpu)
+#define MBOX_DONE_MSK_REG(cpu) 	MAILBOX_DONE_OFFSET + MBOX_SET_MSK_REG(cpu)
+#define MBOX_DONE_INT_REG(cpu) 	MAILBOX_DONE_OFFSET + MBOX_SET_INT_REG(cpu)
+
+#define MBOX_SET_REG 0x60
 
 #define SEND_TO_CPU  1
 #define RECEIVE_CPU  1 // c906B
@@ -93,22 +50,21 @@ static irqreturn_t cv1800b_mbox_isr(int irq, void *dev_id)
 {
 	size_t i;
 	struct cv1800b_mbox *mb = (struct cv1800b_mbox *)dev_id;
-	struct mailbox_done_register *mbox_done_reg = 
-		(struct mailbox_done_register*)(mb->mbox_base + MAILBOX_DONE_OFFSET);
+	u8 done = readb(mb->mbox_base + MBOX_DONE_REG(RECEIVE_CPU));
 	
-	printk("cv1800b_mbox_isr:%d\n", irq);
+	printk("cv1800b_mbox_isr:%d, MBOX_DONE_REG:%d \n", irq, done);
 
 	for (i = 0; i < MAILBOX_MAX_CHAN; i++) {
 		if (mb->content[i]) {
 			printk("cv1800b_mbox_isr, content: %ld\n", i);
-			// [TO OPEN] mbox_chan_received_data(&mb->chans[i], mb->content[i]);
-
-			// for test
-			*mb->content[i] = *mb->content[i]+0xff;
-
+			mbox_chan_received_data(&mb->chans[i], mb->content[i]);
 			mb->content[i] = NULL;
-			//
-			mbox_done_reg->cpu_mbox_done_en[RECEIVE_CPU].mbox_info |= (short)(1<<i);
+			
+			// [FIXME]enable tx done interrupt 
+			// writeb((short)(1<<i) | done, mb->mbox_base + MBOX_DONE_REG(RECEIVE_CPU)); 
+			
+			printk("cv1800b_mbox_isr: MBOX_DONE_REG:%d\n", readb(mb->mbox_base + MBOX_DONE_REG(RECEIVE_CPU)));
+
 			return IRQ_HANDLED;
 		}
 	}
@@ -119,17 +75,14 @@ static irqreturn_t cv1800b_mbox_isr(int irq, void *dev_id)
 static irqreturn_t cv1800b_mbox_irq(int irq, void *dev_id)
 {
 	struct cv1800b_mbox *mb = (struct cv1800b_mbox *)dev_id;
-	struct mailbox_set_register *mbox_reg = mb->mbox_base;
-	struct mailbox_done_register *mbox_done_reg = 
-		(struct mailbox_done_register*)(mb->mbox_base + MAILBOX_DONE_OFFSET);
 	u8 set, done, valid;
 	int i;
 	u64 *buf;
 
 	printk("cv1800b_mbox_irq:%d\n",irq);
 
-	set = mbox_reg->cpu_mbox_set[RECEIVE_CPU].cpu_mbox_int_int.mbox_int;
-	done = mbox_done_reg->cpu_mbox_done[RECEIVE_CPU].cpu_mbox_int_int.mbox_int;
+	set = readb(mb->mbox_base + MBOX_SET_INT_REG(RECEIVE_CPU)); // set = mbox_reg->cpu_mbox_set[RECEIVE_CPU].cpu_mbox_int_int.mbox_int;
+	done = readb(mb->mbox_base + MBOX_DONE_INT_REG(RECEIVE_CPU)); // done = mbox_done_reg->cpu_mbox_done[RECEIVE_CPU].cpu_mbox_int_int.mbox_int;
 	printk("set: %d, done: %d\n", set, done);
 
 	if (set) {
@@ -137,27 +90,27 @@ static irqreturn_t cv1800b_mbox_irq(int irq, void *dev_id)
 			valid = set & (1 << i);
 			if (valid) {
 				buf = (u64*)(mb->mbox_base + MAILBOX_CONTEXT_OFFSET) + i;
-				printk("mailbox: %d, value: 0x%llX\n", i, *buf);
+				printk("cv1800b_mbox_irq: mailbox: %d, set value: 0x%llX\n", i, *buf);
 				mb->content[i] = buf;
 				/* mailbox buffer context is send from rtos, clear mailbox interrupt */
-				mbox_reg->cpu_mbox_set[RECEIVE_CPU].cpu_mbox_int_clr.mbox_int_clr = valid;
+				writeb(valid, mb->mbox_base + MBOX_SET_CLR_REG(RECEIVE_CPU)); // mbox_reg->cpu_mbox_set[RECEIVE_CPU].cpu_mbox_int_clr.mbox_int_clr = valid;
 				// need to disable enable bit
-				mbox_reg->cpu_mbox_en[RECEIVE_CPU].mbox_info &= ~valid;
+				writeb(~valid, mb->mbox_base + MBOX_EN_REG(RECEIVE_CPU)); // mbox_reg->cpu_mbox_en[RECEIVE_CPU].mbox_info &= ~valid;
 
 				return IRQ_WAKE_THREAD;
 			}
 		}
 		
-	} else if (done) {
+	} else if (done) {	// [unused]
 		for (i = 0; i < MAILBOX_MAX_CHAN; i++) {
 			valid = done & (1 << i);
 			if (done & (1 << i)) {
-				printk("mailbox: %d tx done\n", i);
+				printk("cv1800b_mbox_irq: mailbox: %d tx done\n", i);
 				// clr interrupt
-				mbox_done_reg->cpu_mbox_done[RECEIVE_CPU].cpu_mbox_int_clr.mbox_int_clr = valid;
+				writeb(valid, mb->mbox_base + MBOX_DONE_CLR_REG(RECEIVE_CPU)); // mbox_done_reg->cpu_mbox_done[RECEIVE_CPU].cpu_mbox_int_clr.mbox_int_clr = valid;
 				// need to disable enable bit
-				mbox_done_reg->cpu_mbox_done_en[RECEIVE_CPU].mbox_info &= ~valid;
-				// callback for tx done here?
+				writeb(~valid, mb->mbox_base + MBOX_DONE_REG(RECEIVE_CPU)); // mbox_done_reg->cpu_mbox_done_en[RECEIVE_CPU].mbox_info &= ~valid;
+				// [TODO] callback for tx done here?
 				return IRQ_HANDLED;
 			}
 		}
@@ -169,7 +122,6 @@ static irqreturn_t cv1800b_mbox_irq(int irq, void *dev_id)
 
 static int cv1800b_mbox_send_data(struct mbox_chan *chan, void *data) {
 	struct cv1800b_mbox *mbox = dev_get_drvdata(chan->mbox->dev);
-	struct mailbox_set_register *mbox_reg = mbox->mbox_base;
 	int idx = (int)chan->con_priv;
 	u64 *buf = (u64*)(mbox->mbox_base + MAILBOX_CONTEXT_OFFSET) + idx;
 	dev_info(chan->mbox->dev, "cv1800b_mbox_send_data, chan: %d, msg:%llX\n", idx, *((u64*)data));
@@ -177,10 +129,17 @@ static int cv1800b_mbox_send_data(struct mbox_chan *chan, void *data) {
 	// write data
 	*buf = *((u64*)data);
 	// clear mailbox
-	mbox_reg->cpu_mbox_set[SEND_TO_CPU].cpu_mbox_int_clr.mbox_int_clr = (1 << idx);
-	// trigger mailbox valid to rtos
-	mbox_reg->cpu_mbox_en[SEND_TO_CPU].mbox_info |= (1 << idx);
-	mbox_reg->mbox_set.mbox_set = (1 << idx);
+	writeb((short)(1 << idx), mbox->mbox_base + MBOX_SET_CLR_REG(SEND_TO_CPU)); // mbox_reg->cpu_mbox_set[SEND_TO_CPU].cpu_mbox_int_clr.mbox_int_clr = (1 << idx);
+	// trigger mailbox valid
+	u8 en = readb(mbox->mbox_base + MBOX_EN_REG(SEND_TO_CPU));
+	writeb(en | (1 << idx), mbox->mbox_base + MBOX_EN_REG(SEND_TO_CPU)); // mbox_reg->cpu_mbox_en[SEND_TO_CPU].mbox_info |= (1 << idx);
+	writeb((short)(1 << idx), mbox->mbox_base + MBOX_SET_REG); // mbox_reg->mbox_set.mbox_set = (1 << idx);
+
+	if (chan->cl->tx_block) {
+		printk("complete(&chan->tx_complete);\n");
+		complete(&chan->tx_complete);
+	}
+
 	return 0;
 }
 
@@ -192,7 +151,7 @@ static const struct mbox_chan_ops cv1800b_mbox_chan_ops = {
 };
 
 static const struct of_device_id cv1800b_mbox_of_match[] = {
-	{ .compatible = "cvitek,rtos_cmdqu", },
+	{ .compatible = "cvitek,cv180x-mailbox", },
 	{},
 };
 MODULE_DEVICE_TABLE(of, cv1800b_mbox_of_match);
@@ -264,7 +223,7 @@ static struct platform_driver cv1800b_mbox_driver = {
 
 static int __init cv1800b_mbox_init(void)
 {
-	printk("cv1800b_mbox_init -- 2023-11-13\n");
+	printk("cv1800b_mbox_init -- 2023-12-17\n");
 	return platform_driver_register(&cv1800b_mbox_driver);
 }
 core_initcall(cv1800b_mbox_init);
